71e1d97f81632f8fa306f46eb351f1175d60b480
oacikel <41515954+oacikel@users.noreply.github.com>
2021-02-02 13:05:23 +0300

    handled toolbar menu favourite icon
    icon sets to empty star or full star according to whether repo is within favourited icons
    
    icon switch from empty to full or vice-versa after menu item click

--- app/src/main/java/com/ocul/githubrepolisting/view/fragments/repoDetail/RepoDetailFragment.kt
+++ app/src/main/java/com/ocul/githubrepolisting/view/fragments/repoDetail/RepoDetailFragment.kt
@@ -1,16 +1,13 @@
 package com.ocul.githubrepolisting.view.fragments.repoDetail
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.content.ContentProviderCompat
+import android.util.Log
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.ocul.githubrepolisting.R
 import com.ocul.githubrepolisting.databinding.FragmentRepoDetailBinding
-import com.ocul.githubrepolisting.repository.PrefRepository
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.fragment_repo_detail.*
@@ -18,8 +15,10 @@ import org.jetbrains.anko.appcompat.v7.coroutines.onMenuItemClick
 
 
 class RepoDetailFragment : Fragment() {
+    private val LOG_TAG = "OCULCAN - RepoDetailFragment"
     private lateinit var viewModel: RepoDetailFragmentViewModel
     private lateinit var binding: FragmentRepoDetailBinding
+    private lateinit var menu:Menu
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,14 +38,26 @@ class RepoDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         arguments?.let {
-            viewModel.setup(it, view, activity?.toolbar)
+            viewModel.setupFragment(it, view, (parentFragment?.activity as AppCompatActivity).toolbar,this)
         }
+        Log.d(LOG_TAG,"Menu has visible items (before item click): "+activity?.toolbar?.menu?.hasVisibleItems())
+
         activity?.toolbar?.onMenuItemClick {
+            Log.d(LOG_TAG,"Menu has visible items: "+activity?.toolbar?.menu?.hasVisibleItems())
             when (it?.itemId) {
                 R.id.action_favorite -> {
                     viewModel.changeRepoSavedStatus()
+                    viewModel.changeMenuIcon(it)
                 }
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        this.menu =menu
+        arguments?.let {
+            viewModel.setupMenuItems(menu,this)
         }
     }
 }
--- app/src/main/java/com/ocul/githubrepolisting/view/fragments/repoDetail/RepoDetailFragmentViewModel.kt
+++ app/src/main/java/com/ocul/githubrepolisting/view/fragments/repoDetail/RepoDetailFragmentViewModel.kt
@@ -1,31 +1,46 @@
 package com.ocul.githubrepolisting.view.fragments.repoDetail
 
 import android.app.Application
-import android.content.Context
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.core.view.get
-import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 import com.ocul.githubrepolisting.R
 import com.ocul.githubrepolisting.model.Owner
 import com.ocul.githubrepolisting.model.RepoItem
 import com.ocul.githubrepolisting.repository.PrefRepository
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.activity_main.view.*
-import kotlinx.android.synthetic.main.fragment_repo_detail.*
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_repo_detail.view.*
+import kotlin.coroutines.coroutineContext
 
 class RepoDetailFragmentViewModel(application: Application) : AndroidViewModel(application) {
     private val LOG_TAG = "OCULCAN - RepoDetailFragmentViewModel"
     private val prefRepository: PrefRepository = PrefRepository(application)
+    private lateinit var drawbleFavourited: Drawable
+    private lateinit var drawbleNotFavourited: Drawable
+
 
     private lateinit var selectedRepo: RepoItem
-    fun setup(arguments: Bundle, view: View, toolbar: Toolbar?) {
+    fun setupMenuItems(menu: Menu, repoDetailFragment: RepoDetailFragment) {
+        val view: View? =repoDetailFragment.view
+        drawbleFavourited = view?.context!!.resources.getDrawable(R.drawable.icon_favorite,view.context!!.theme)
+        drawbleNotFavourited = view?.context!!.resources.getDrawable(R.drawable.icon_favourite_empty,view.context!!.theme)
+        if (prefRepository.getFavouriteIds()?.contains(selectedRepo.id.toString()) == true) {
+            Log.d(LOG_TAG, "Changing logo to full star.")
+           menu.findItem(R.id.action_favorite )?.icon=drawbleFavourited
+        } else {
+            Log.d(LOG_TAG, "Changing logo to empty star.")
+            menu.findItem(R.id.action_favorite )?.icon=drawbleNotFavourited
+        }
+    }
+    fun setupFragment(arguments: Bundle, view: View, toolbar: Toolbar?, fragment: Fragment?) {
         Log.d(LOG_TAG, "Executing setup function.")
         val id = arguments.let { RepoDetailFragmentArgs.fromBundle(it).id }
         val starCount = arguments.let { RepoDetailFragmentArgs.fromBundle(it).starCount }
@@ -41,20 +56,23 @@ class RepoDetailFragmentViewModel(application: Application) : AndroidViewModel(a
         view.textViewStarCount.setText(selectedRepo.stargazers_count.toString())
         toolbar?.setTitle(selectedRepo.name)
         Picasso.get().load(selectedRepo.owner.avatar_url).into(view.imageViewAvatar);
-        val drawbleFavourited = view.context!!.resources.getDrawable(R.drawable.icon_favorite,view.context!!.theme)
-        val drawbleNotFavourited = view.context!!.resources.getDrawable(R.drawable.icon_favourite_empty,view.context!!.theme)
-
-        if (prefRepository.getFavouriteIds()?.contains(selectedRepo.id.toString()) == true) {
-            toolbar?.menu?.get(0)?.icon=drawbleFavourited
-        } else {
-            toolbar?.menu?.get(0)?.icon=drawbleNotFavourited
-        }
     }
 
     fun changeRepoSavedStatus() {
         Log.d(LOG_TAG, "Executing changeRepoSavedStatus.")
         prefRepository.handleFavouriteId(selectedRepo.id.toString())
         prefRepository.getFavouriteIds()?.forEach { Log.d(LOG_TAG, it) }
+    }
+
+    fun changeMenuIcon(item: MenuItem) {
+        when (item.icon) {
+            drawbleFavourited -> {
+                item.icon=drawbleNotFavourited
+            }
+            drawbleNotFavourited -> {
+                item.icon=drawbleFavourited
+            }
+        }
     }
 
 
